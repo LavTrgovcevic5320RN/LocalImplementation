a04e74763f4a5654a469e78cbaf9094b4e5ba746
Lav Trgovčević RN 53/20 <ltrgovcevic5320rn@raf.rs>
2022-10-29 20:47:27 +0200

    Spojena implementacija sa specifikacijom

--- src/main/java/LocalImplementation.java
+++ src/main/java/LocalImplementation.java
@@ -1,13 +1,61 @@
+import exceptions.FileException;
 import exceptions.InvalidConstraintException;
+import storage.FileMetaData;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 
 public class LocalImplementation implements Storage{
 
-    @Override
-    public void initialiseStorage(String s, String s1) {
+    FileMetaData rootFolder = null;
 
+
+    @Override
+    public void initialiseDirectory(String storageName, String path, int size, int maxFiles, String...st ) {
+        File directory = new File(path + "\\" + storageName);
+        System.out.println(directory.getPath());
+        if (size != 0) {
+            rootFolder = new FileMetaData(storageName, path, new Date(), new Date(), 1024, FileMetaData.Type.DIRECTORY);
+        } else {
+            rootFolder = new FileMetaData(storageName, path, new Date(), new Date(), size, FileMetaData.Type.DIRECTORY);
+        }
+
+        if (maxFiles != 0) {
+            rootFolder.setMaxNumberOfFiles(maxFiles);
+        }
+
+        if (st.length > 0) {
+            for (String s : st) {
+                ArrayList<String> arrayList = (ArrayList<String>) rootFolder.getIllegalExtensions();
+                arrayList.add(s);
+                rootFolder.setIllegalExtensions(arrayList);
+            }
+
+            if (storageName.contains(".")) {
+                String ekstenzija = storageName.substring(storageName.indexOf(".") + 1);
+                for (String s : rootFolder.getIllegalExtensions()) {
+//                System.out.println(s);
+                    if (s.equals(ekstenzija))
+                        try {
+                            throw new Exception();
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                }
+            }
+
+            if (!rootFolder.getIllegalExtensions().isEmpty()) {
+                for (String s : rootFolder.getIllegalExtensions())
+                    if (storageName.endsWith(s))
+                        throw new FileException("greska sa fajlom");
+            }
+            directory.mkdir();
+//        System.out.println(directory.isDirectory());
+//        System.out.println(directory.getName());
+
+        }
     }
 
     @Override
@@ -21,12 +69,19 @@ public class LocalImplementation implements Storage{
     }
 
     @Override
-    public void createDirectory(String s, String s1) {
+    public void create(String storageName, String path) {
+        File root = new File(path + "/" + storageName);
+        rootFolder = new FileMetaData(storageName, path, new Date(), new Date(), 0 , FileMetaData.Type.DIRECTORY);
+        root.mkdirs();
 
+        System.out.println(root.getPath() + " \n " + root.getName());
+        rootFolder.setByteSize(10);
+
+         
     }
 
     @Override
-    public void createDirectory(String s, String s1, String s2) {
+    public void create(String s, String s1, String s2) {
 
     }
 
