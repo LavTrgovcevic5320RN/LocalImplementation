dcc724ff937a8a9f309745d0fbac1bc97f24ec49
Lav Trgovčević RN 53/20 <ltrgovcevic5320rn@raf.rs>
2022-11-26 15:23:28 +0100

    zakomentarisano

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,39 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
 
-
-
     public static void main(String[] args) throws IOException {
-        LocalImplementation l = LocalImplementation.getInstance();
-        l.initialiseDirectory("SK", "C:\\Users\\Lav\\Desktop\\Adasdasd", 2048, 5,  "exe");
-        l.create("A", "C:\\Users\\Lav\\Desktop\\Adasdasd\\SK\\", 5);
-        l.create("B", "C:\\Users\\Lav\\Desktop\\Adasdasd\\SK\\", 5);
-        File file1 = new File("C:\\Users\\Lav\\Desktop\\Adasdasd\\SK\\A\\1.txt");
-        File file2 = new File("C:\\Users\\Lav\\Desktop\\Adasdasd\\SK\\A\\2.xlsx");
-        file1.createNewFile();
-        file2.createNewFile();
+//        LocalImplementation l = LocalImplementation.getInstance();
+//        l.initialiseDirectory("SK", "C:\\Users\\Lav\\Desktop\\Adasdasd", 2048, 5,  "exe");
+//        l.openDirectory("C:/Users/Lav/Desktop/Adasdasd/SK");
+//        l.create("A", "#/", 5);
+//       l.create("B", "#/", 5);
+//        File file1 = new File("C:/Users/Lav/Desktop/Adasdasd/SK/A/1.txt");
+//        File file2 = new File("C:/Users/Lav/Desktop/Adasdasd/SK/A/2.xlsx");
+//        file1.createNewFile();
+//        file2.createNewFile();
+//
+//        l.create("B", "#/A/", 5);
+//        l.create("C", "#/A/B/", 5);
+//        File file3 = new File("C:/Users/Lav/Desktop/Adasdasd/SK/A/B/1.txt");
+//        File file4 = new File("C:/Users/Lav/Desktop/Adasdasd/SK/A/B/C/2.xlsx");
+//        file3.createNewFile();
+//        file4.createNewFile();
 
-        l.rename("Marko", "C:\\Users\\Lav\\Desktop\\Adasdasd\\SK\\A");
 
 
-        //l.moveFiles("#\\B", "#\\A\\1.txt", "#\\A\\2.xlsx");
+
+//        l.download( "C:/Users/Lav/Desktop/Proba", "#/A");
+//        l.download( "C:/Users/Lav/Desktop/Proba", "#/A");
+
+//        //l.rename("Marko", "C:\\Users\\Lav\\Desktop\\Adasdasd\\SK\\A");
+//        l.moveFiles("#/B", "#/A");      //primer za move skladista za fajlovima
+//        l.moveFiles("#/B", "#/A/1.docx");   // primer za move samo fajlova
+
 //        System.out.println(BraceExpansion.expand("C:/{a,b,c}/{1..15}adc"));
+//        System.out.println(BraceExpansion.expand("C:\\1\\{Z..A}\\{1..3}\\{a..r}"));
+
     }
 
 
